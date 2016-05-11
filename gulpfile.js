b5e3ffac0906f6dfc094cc675bf7d1ddf3e3973e
Maxime Rouiller <angelzerosoft@gmail.com>
2016-05-11 16:03:27 -0400

    removed duplication

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,21 @@
 var gulp = require('gulp');
 var resume = require('gulp-resume');
 var rename = require('gulp-rename');
+var option = {
+    format: 'html',
+    theme: 'paper'
+};
 
 gulp.task('resume-en', function () {
     return gulp.src('en.json')
-        .pipe(resume({
-            format: 'html',
-            theme: 'slick'
-        }))
+        .pipe(resume(option))
         .pipe(rename('en.html'))
         .pipe(gulp.dest('./output'));
 });
 
 gulp.task('resume-fr', function () {
     return gulp.src('fr.json')
-        .pipe(resume({
-            format: 'html',
-            theme: 'slick'
-        }))
+        .pipe(resume(option))
         .pipe(rename('fr.html'))
         .pipe(gulp.dest('./output'));
 });
