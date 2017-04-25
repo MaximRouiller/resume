f418ef9fd9ecb9ed79e4d80ce5add3a11dcf9ab3
Maxime Rouiller <angelzerosoft@gmail.com>
2017-04-25 08:12:31 -0400

    refactored the code to a bit more modular and less single-filed

--- index.js
+++ index.js
@@ -1,106 +1,38 @@
-(function () {
-    var fs = require("fs");
-    var path = require("path");
-    var Handlebars = require("handlebars");
-    var moment = require("moment");
-    var phantomjs = require('phantomjs-prebuilt')
+(function() {
+  var fs = require("fs");
+  var path = require("path");
+  var Handlebars = require("handlebars");
+  var handlebarsHelpers = require("./template/HandlebarHelpers");
+  var phantomjs = require('phantomjs-prebuilt')
 
-    var base = require("./MaximeRouiller.base.json");
-    var english = require("./MaximeRouiller.en.json");
-    var french = require("./MaximeRouiller.fr.json");
+  var base = require("./MaximeRouiller.base.json");
+  var english = require("./MaximeRouiller.en.json");
+  var french = require("./MaximeRouiller.fr.json");
 
-    var compiled = __dirname + '/compiled';
-    if (!fs.existsSync(compiled)) {
-        fs.mkdirSync(compiled, 0744);
-    }
+  var compiled = __dirname + '/compiled';
+  if (!fs.existsSync(compiled)) {
+    fs.mkdirSync(compiled, 0744);
+  }
+  handlebarsHelpers.register();
 
-    // unnecessary. helpful for debugging however.
-    // fs.writeFile(compiled + "/MaximeRouiller.en.json", JSON.stringify(Object.assign(base, english)), {
-    //     flag: 'w'
-    // });
-    // fs.writeFile(compiled + "/MaximeRouiller.fr.json", JSON.stringify(Object.assign(base, french)), {
-    //     flag: 'w'
-    // });
+  function render(resume, filename) {
+    var css = fs.readFileSync(__dirname + "/template/style.css", "utf-8");
+    var tpl = fs.readFileSync(__dirname + "/template/resume.hbs", "utf-8");
 
-    function render(resume, filename) {
-        var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-        var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-
-        //generates HTML
-        fs.writeFile(compiled + "/" + filename + ".html", Handlebars.compile(tpl)({
-            css: css,
-            resume: resume
-        }), {
-            flag: 'w'
-        });
-
-        // generates PDF
-        var program = phantomjs.exec('./rasterize.js', './compiled/' + filename + '.html', './compiled/' + filename + '.pdf', 'Letter')
-        program.stdout.pipe(process.stdout);
-        program.stderr.pipe(process.stderr);
-    }
-
-    /* HANDLEBARS HELPERS */
-    Handlebars.registerHelper('paragraphSplit', function (plaintext) {
-        var output = '';
-        var lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
-        var i = 0;
-
-        while (i < lines.length) {
-            if (lines[i]) {
-                output += '<p>' + lines[i] + '</p>';
-            }
-            i += 1;
-        }
-
-        return new Handlebars.SafeString(output);
+    //generates HTML
+    fs.writeFile(compiled + "/" + filename + ".html", Handlebars.compile(tpl)({
+      css: css,
+      resume: resume
+    }), {
+      flag: 'w'
     });
 
-    Handlebars.registerHelper('toLowerCase', function (str) {
-        return str.toLowerCase();
-    });
+    // generates PDF
+    var program = phantomjs.exec('./scripts/rasterize.js', './compiled/' + filename + '.html', './compiled/' + filename + '.pdf', 'Letter')
+    program.stdout.pipe(process.stdout);
+    program.stderr.pipe(process.stderr);
+  }
 
-    Handlebars.registerHelper('spaceToDash', function (str) {
-        return str.replace(/\s/g, '-').toLowerCase();
-    });
-
-    Handlebars.registerHelper('MY', function (date) {
-        return moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY');
-    });
-
-    Handlebars.registerHelper('Y', function (date) {
-        return moment(date.toString(), ['YYYY-MM-DD']).format('YYYY');
-    });
-
-    Handlebars.registerHelper('DMY', function (date) {
-        return moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY');
-    });
-
-    Handlebars.registerHelper('birthData', function (birth) {
-        var out = [];
-        if (birth && Object.keys(birth).length) {
-            out.push('<div> Born in ');
-            out.push(birth.place);
-
-            if (birth.place && birth.state) {
-                out.push(', ');
-            }
-
-            out.push(birth.state);
-
-            var year = birth.date &&
-                moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY');
-
-            if (year && (birth.place || birth.state)) {
-                out.push(' in ');
-            }
-            out.push(year);
-            out.push('</div>');
-        }
-
-        return new Handlebars.SafeString(out.join(''));
-    });
-
-    render(Object.assign(base, english), 'MaximeRouiller.en');
-    render(Object.assign(base, french), 'MaximeRouiller.fr');
+  render(Object.assign(base, english), 'MaximeRouiller.en');
+  render(Object.assign(base, french), 'MaximeRouiller.fr');
 }());
---
+++ template/HandlebarHelpers.js
@@ -0,0 +1,72 @@
+(function() {
+  var moment = require('moment');
+
+  var Handlebars = require("handlebars");
+
+  function register() {
+    /* HANDLEBARS HELPERS */
+    Handlebars.registerHelper('paragraphSplit', function(plaintext) {
+      var output = '';
+      var lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
+      var i = 0;
+
+      while (i < lines.length) {
+        if (lines[i]) {
+          output += '<p>' + lines[i] + '</p>';
+        }
+        i += 1;
+      }
+
+      return new Handlebars.SafeString(output);
+    });
+
+    Handlebars.registerHelper('toLowerCase', function(str) {
+      return str.toLowerCase();
+    });
+
+    Handlebars.registerHelper('spaceToDash', function(str) {
+      return str.replace(/\s/g, '-').toLowerCase();
+    });
+
+    Handlebars.registerHelper('MY', function(date) {
+      return moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY');
+    });
+
+    Handlebars.registerHelper('Y', function(date) {
+      return moment(date.toString(), ['YYYY-MM-DD']).format('YYYY');
+    });
+
+    Handlebars.registerHelper('DMY', function(date) {
+      return moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY');
+    });
+
+    Handlebars.registerHelper('birthData', function (birth) {
+        var out = [];
+        if (birth && Object.keys(birth).length) {
+            out.push('<div> Born in ');
+            out.push(birth.place);
+
+            if (birth.place && birth.state) {
+                out.push(', ');
+            }
+
+            out.push(birth.state);
+
+            var year = birth.date &&
+                moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY');
+
+            if (year && (birth.place || birth.state)) {
+                out.push(' in ');
+            }
+            out.push(year);
+            out.push('</div>');
+        }
+
+        return new Handlebars.SafeString(out.join(''));
+    });
+  }
+
+  module.exports = {
+    register: register
+  };
+})();
